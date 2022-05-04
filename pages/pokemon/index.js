5a3c9dfa12ab8bb2829ec16a73e86e16d525af47
Jack <loaizajack@gmail.com>
2022-05-04 21:24:07 +0100

    collecting relevent data for filters and apply filters

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,14 +1,57 @@
 import {getAllPokemon, getSinglePokemon} from "../../lib/fetch";
 
-import {SimpleGrid, Container} from '@chakra-ui/react'
+import {SimpleGrid, Container, Divider, Select} from '@chakra-ui/react'
 import InfoCard from "../../components/InfoCard";
+import {useEffect, useState} from "react";
+import {getGenerationNames, getTypeNames} from "../../lib/utility";
 
-export default function AllPokemon({allPokemon}) {
+export default function AllPokemon({allPokemon, types, generations}) {
+    const [typeFilter, setTypeFilter] = useState(null)
+    const [generationFilter, setGenerationFilter] = useState(null)
+
+    function filteredPokemon() {
+        if (!typeFilter && !generationFilter) {
+            return allPokemon;
+        } else if (typeFilter && !generationFilter) {
+            return allPokemon.filter(pokemon => {
+                pokemon.types.includes(typeFilter);
+            })
+        } else if (!typeFilter && generationFilter) {
+            return allPokemon.filter(pokemon => {
+                return pokemon.generation === generationFilter;
+            })
+        } else {
+            return allPokemon.filter(pokemon => {
+                return (
+                    pokemon.types.includes(typeFilter)
+                    && pokemon.generation === generationFilter
+                );
+            })
+        }
+    }
+
     return (
         <>
+            <Container maxW="container.lg" display="flex">
+                <Select onChange={e => setTypeFilter(e.target.value === '0'? null:e.target.value)}>
+                    <option value={0}>Filter Type</option>
+                    {types.map((type, key) => (
+                        <option key={key}>{type}</option>
+                    ))}
+                </Select>
+                <Select onChange={e => setGenerationFilter(e.target.value === '0'? null:e.target.value)}>
+                    <option value={0}>Filter Generation</option>
+                    {generations.map((generation, key) => (
+                        <option key={key}>{generation}</option>
+                    ))}
+                </Select>
+            </Container>
+            <Container maxW="container.lg" my="10px">
+                <Divider/>
+            </Container>
             <Container maxW="container.lg">
                 <SimpleGrid columns={4} spacing={10}>
-                    {allPokemon.map((pokemon, key) => (
+                    {filteredPokemon().map((pokemon, key) => (
                         <InfoCard key={key} {...pokemon} useLink={true}/>
                     ))}
                 </SimpleGrid>
@@ -19,24 +62,34 @@ export default function AllPokemon({allPokemon}) {
 
 export async function getStaticProps(context) {
     let allPokemon = [];
-    let allBasic = (await getAllPokemon()).results
+    const [allBasic, types, generations] = await Promise.all([
+        getAllPokemon(),
+        getTypeNames(),
+        getGenerationNames()
+    ])
 
-    for (let key in allBasic) {
-        let single = await getSinglePokemon(allBasic[key].name);
 
+    for (let pokeKey in allBasic.results) {
+        let [pokemon, pokemonSpecies] = await getSinglePokemon(allBasic.results[pokeKey].name);
         allPokemon.push({
-            name: single.species.name,
-            image: single.sprites.front_default,
+            name: pokemon.species.name,
+            image: pokemon.sprites.front_default,
+            types: pokemon.types.map((type) => {
+                return type.type.name
+            }),
+            generation: pokemonSpecies.generation.name
         })
 
-        if (process.env.DEVELOPMENT && allPokemon.length > 100) {
+        if (process.env.DEVELOPMENT && allPokemon.length > 10) {
             break;
         }
     }
 
     return {
         props: {
-            allPokemon
+            allPokemon,
+            types,
+            generations
         },
     }
 }
